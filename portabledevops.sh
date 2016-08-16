f4f3e1542dea710aca71fa52fd9ee55906cd101f
robertluwang <robert.lu.wang@gmail.com>
2016-08-16 11:13:45 -0400

    renamed portable tool to short name

--- portabledevops.sh
+++ portabledevops.sh
@@ -15,25 +15,26 @@ export HOME=/home/$USERNAME
 export DEVOPS=$HOMEDRIVE/portabledevops
 
 # production tool 
-alias 7zp='/$HOMEDRIVEL/portabledevops/7-ZipPortable/7-ZipPortable.exe'
-alias img='/$HOMEDRIVEL/portabledevops/ImgBurnPortable/ImgBurn.exe'
-alias fzp='/$HOMEDRIVEL/portabledevops/FileZillaPortable/FileZillaPortable.exe'
-alias qdir='/$HOMEDRIVEL/portabledevops/Q-Dir_Portable/Q-Dir.exe'
-alias scite='/$HOMEDRIVEL/portabledevops/SciTEPortable/SciTE.exe'
-alias st3='/$HOMEDRIVEL/portabledevops/SublimeText3/sublime_text.exe'
-alias gshot='/$HOMEDRIVEL/portabledevops/Greenshot/Greenshot.exe'
-alias kitty='/$HOMEDRIVEL/portabledevops/Kitty/kitty_portable.exe'
+alias 7zp='/$HOMEDRIVEL/portabledevops/7z/7-ZipPortable.exe'
+alias img='/$HOMEDRIVEL/portabledevops/imgburn/ImgBurn.exe'
+alias fzp='/$HOMEDRIVEL/portabledevops/filezilla/FileZillaPortable.exe'
+alias qdir='/$HOMEDRIVEL/portabledevops/qdir/Q-Dir.exe'
+alias scite='/$HOMEDRIVEL/portabledevops/scite/SciTE.exe'
+alias st3='/$HOMEDRIVEL/portabledevops/sublimetext3/sublime_text.exe'
+alias gshot='/$HOMEDRIVEL/portabledevops/greenshot/Greenshot.exe'
+alias kitty='/$HOMEDRIVEL/portabledevops/kitty/kitty_portable.exe'
 
 # portable calibre tool
-export PATH=/$HOMEDRIVEL/portabledevops/CalibrePortable/Calibre:$PATH
+export PATH=/$HOMEDRIVEL/portabledevops/calibre/Calibre:$PATH
+alias calibrep='/$HOMEDRIVEL/portabledevops/calibre/calibre-portable.exe'
 
 # portable docker toolbox
-export DOCKERTOOLBOX=/$HOMEDRIVEL/portabledevops/portableDockerToolbox
+export DOCKERTOOLBOX=/$HOMEDRIVEL/portabledevops/dockertoolbox
 export PATH=$DOCKERTOOLBOX:$PATH
 alias dockerstart='cd $DOCKERTOOLBOX;./start.sh'
 
 # portable virtualbox 
-export VIRTUALBOX=/$HOMEDRIVEL/portabledevops/VirtualBox
+export VIRTUALBOX=/$HOMEDRIVEL/portabledevops/virtualbox
 export PATH=$VIRTUALBOX:$PATH
 
 # portable python
@@ -48,7 +49,7 @@ fi
 export GOPATH=$HOME/testgo
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
-# portable lua
+# portable Lua
 export LUA_DEV=/$HOMEDRIVEL/portabledevops/Lua/5.1
 export LUA_PATH=/$HOMEDRIVEL/portabledevops/Lua/5.1/lua/?.luac
 export PATH=$LUA_DEV:$LUA_DEV/clibs:$PATH
@@ -62,8 +63,8 @@ export PATH=/$HOMEDRIVEL/portabledevops/nodejs:/$HOMEDRIVEL/portabledevops/nodej
 # portable git and bash
 CMDERVER=`echo $CMDER_ROOT| awk -F/ '{print $NF}'`
 if [ $CMDERVER == cmdermini ]; then 
-	export PATH=/$HOMEDRIVEL/portabledevops/PortableGit/bin:$PATH
-fi
+	export PATH=/$HOMEDRIVEL/portabledevops/git/bin:$PATH
+fi 
 
 # portable mingw
 export PATH=/$HOMEDRIVEL/portabledevops/mingw/bin:$PATH
@@ -73,7 +74,7 @@ alias cmdermini='/$HOMEDRIVEL/portabledevops/cmdermini/cmder.exe'
 alias cmder='/$HOMEDRIVEL/portabledevops/cmder/cmder.exe'
 
 # portable console2
-alias console='/$HOMEDRIVEL/portabledevops/Console2/Console.exe'
+alias console='/$HOMEDRIVEL/portabledevops/console2/Console.exe'
 
 cd $HOME
 
