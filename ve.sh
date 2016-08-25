08a7f9339b7e4cd2fbe58c97d1bccffc2f195576
robertluwang <robert.lu.wang@gmail.com>
2016-08-25 13:00:32 -0400

    add ve.sh, activate replacemenet for virtualenv

---
+++ ve.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ve.sh activate replacement for virtualenv 
+# By Robert Wang
+# Aug 25, 2016
+# Usage: place ve.sh to virtualenv project folder and source it
+# $ source ./ve.sh 
+
+export VIRTUAL_ENV=`pwd`
+vename=`pwd | rev |cut -d/ -f1| rev`
+export PATH="$VIRTUAL_ENV/Scripts:$PATH"
+alias mypip="$VIRTUAL_ENV/python $VIRTUAL_ENV/Scripts/pip.exe"
+alias myipy="$VIRTUAL_ENV/python $VIRTUAL_ENV/Scripts/ipython.exe"
+alias mynb="$VIRTUAL_ENV/python $VIRTUAL_ENV/Scripts/jupyter-notebook.exe"
+alias myeasy="$VIRTUAL_ENV/python $VIRTUAL_ENV/Scripts/easy_install.exe"
+export PS1='($vename) \u> \w
+$ '
