63297f0a8827c83144e2474f1a3be69f1ecb2e0f
robertluwang <robert.lu.wang@gmail.com>
2016-08-25 13:17:06 -0400

    correct python path for ve.sh

--- ve.sh
+++ ve.sh
@@ -3,14 +3,14 @@
 # By Robert Wang
 # Aug 25, 2016
 # Usage: place ve.sh to virtualenv project folder and source it
-# $ source ./ve.sh 
+# $ source ve.sh 
 
 export VIRTUAL_ENV=`pwd`
 vename=`pwd | rev |cut -d/ -f1| rev`
 export PATH="$VIRTUAL_ENV/Scripts:$PATH"
-alias mypip="$VIRTUAL_ENV/python $VIRTUAL_ENV/Scripts/pip.exe"
-alias myipy="$VIRTUAL_ENV/python $VIRTUAL_ENV/Scripts/ipython.exe"
-alias mynb="$VIRTUAL_ENV/python $VIRTUAL_ENV/Scripts/jupyter-notebook.exe"
-alias myeasy="$VIRTUAL_ENV/python $VIRTUAL_ENV/Scripts/easy_install.exe"
+alias mypip="$VIRTUAL_ENV/Scripts/python $VIRTUAL_ENV/Scripts/pip.exe"
+alias myipy="$VIRTUAL_ENV/Scripts/python $VIRTUAL_ENV/Scripts/ipython.exe"
+alias mynb="$VIRTUAL_ENV/Scripts/python $VIRTUAL_ENV/Scripts/jupyter-notebook.exe"
+alias myeasy="$VIRTUAL_ENV/Scripts/python $VIRTUAL_ENV/Scripts/easy_install.exe"
 export PS1='($vename) \u> \w
 $ '
