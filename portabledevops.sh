0ba6dc9a3051152ab13c7208f6f8fc6b5126ebc5
robertluwang <robert.lu.wang@gmail.com>
2017-12-15 10:59:36 -0500

    updated minikubefw()

--- portabledevops.sh
+++ portabledevops.sh
@@ -178,6 +178,15 @@ if [ -d $PORTABLEPATH/dockertoolbox ]; then
         else 
             VBoxManage controlvm minikube natpf1 k8s-apiserver,tcp,127.0.0.1,8443,,8443
         fi 
+
+        check2374=`VBoxManage showvminfo minikube|grep 127.0.0.1|grep 2374|cut -d, -f4|cut -d= -f2`
+        if [ $check2374 == '2374' ]; then 
+            # if 127.0.0.1 2374 forward exist, skip
+            echo 
+        else 
+            VBoxManage controlvm minikube natpf1 k8s-docker,tcp,127.0.0.1,2374,,2376
+        fi
+
         check30000=`VBoxManage showvminfo minikube|grep 127.0.0.1|grep 30000|cut -d, -f4|cut -d= -f2`
         if [ $check30000 == '30000' ]; then 
             # if 127.0.0.1 30000 forward exist, skip
@@ -189,6 +198,11 @@ if [ -d $PORTABLEPATH/dockertoolbox ]; then
         kubectl config set-cluster minikube-vpn --server=https://127.0.0.1:8443 --insecure-skip-tls-verify
         kubectl config set-context minikube-vpn --cluster=minikube-vpn --user=minikube
         kubectl config use-context minikube-vpn  
+
+        eval $(minikube docker-env) 
+        unset DOCKER_TLS_VERIFY
+        export DOCKER_HOST="tcp://127.0.0.1:2374"
+        alias docker='docker --tls'
     }
     export -f minikubefw
 
